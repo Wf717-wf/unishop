c98b934e0198d4f6bf078ddc6697606b6547254e
wf <8799741+wf_1999@user.noreply.gitee.com>
2022-09-12 14:45:03 +0800

    完成了home首页的开发

--- main.js
+++ main.js
@@ -1,5 +1,25 @@
 import App from './App'
+import {$http} from '@escook/request-miniprogram'
+uni.$http=$http;
+$http.baseUrl = "https://api-hmugo-web.itheima.net"
+// 请求开始之前做一些事情
+$http.beforeRequest = function (options) {
+  uni.showLoading({
+    title: '数据加载中...',
+  })
+}
 
+// 请求完成之后做一些事情
+$http.afterRequest = function () {
+  uni.hideLoading()
+}
+uni.$showMsg = function (title = '数据加载失败！', duration = 1500) {
+  uni.showToast({
+    title,
+    duration,
+    icon: 'none',
+  })
+}
 // #ifndef VUE3
 import Vue from 'vue'
 Vue.config.productionTip = false
