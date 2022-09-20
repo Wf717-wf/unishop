76b3427e30ee3bae0e8b8011156495d44c31c515
wf <8799741+wf_1999@user.noreply.gitee.com>
2022-09-20 15:46:50 +0800

    完成搜索页面

---
+++ unpackage/dist/dev/mp-weixin/subpkg/search/search.js
@@ -0,0 +1,33 @@
+(global["webpackJsonp"] = global["webpackJsonp"] || []).push([["subpkg/search/search"],{
+
+/***/ 56:
+/*!**********************************************************************************!*\
+  !*** E:/小程序/unishop/unishop/unishop/main.js?{"page":"subpkg%2Fsearch%2Fsearch"} ***!
+  \**********************************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports, __webpack_require__) {
+
+"use strict";
+/* WEBPACK VAR INJECTION */(function(createPage) {__webpack_require__(/*! uni-pages */ 5);
+
+
+var _vue = _interopRequireDefault(__webpack_require__(/*! vue */ 4));
+var _search = _interopRequireDefault(__webpack_require__(/*! ./subpkg/search/search.vue */ 57));function _interopRequireDefault(obj) {return obj && obj.__esModule ? obj : { default: obj };} // @ts-ignore
+wx.__webpack_require_UNI_MP_PLUGIN__ = __webpack_require__;createPage(_search.default);
+/* WEBPACK VAR INJECTION */}.call(this, __webpack_require__(/*! ./node_modules/@dcloudio/uni-mp-weixin/dist/index.js */ 1)["createPage"]))
+
+/***/ }),
+
+/***/ 57:
+/*!***************************************************************!*\
+  !*** E:/小程序/unishop/unishop/unishop/subpkg/search/search.vue ***!
+  \***************************************************************/
+/*! no static exports found */
+/***/ (function(module, exports) {
+
+throw new Error("Module build failed (from ./node_modules/@dcloudio/webpack-uni-mp-loader/lib/style.js):\nError: ENOENT: no such file or directory, open 'E:\\小程序\\unishop\\unishop\\unishop\\subpkg\\search\\search.vue'");
+
+/***/ })
+
+},[[56,"common/runtime","common/vendor"]]]);
+//# sourceMappingURL=../../../.sourcemap/mp-weixin/subpkg/search/search.js.map
